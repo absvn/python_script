d61bb95ed8a1e8aa34eb3282fbd0186cb6ac0a9a
Abhinav Singh <90246402+absvn@users.noreply.github.com>
2021-11-25 17:46:29 +0530

    all pdf inside the folder to image

---
+++ all_pdf_to_image.py
@@ -0,0 +1,11 @@
+from pdf2image import convert_from_path
+import glob,os
+import os, subprocess
+
+os.chdir('/Users/abhinavsingh/Desktop/folder_name/')
+
+for file in glob.glob('*.pdf'):
+    images = convert_from_path(file)
+    
+for i in range(len(images)):
+    images[i].save('/Users/abhinavsingh/Desktop/folder_name/page'+ str(i) +'.jpg', 'JPEG')
