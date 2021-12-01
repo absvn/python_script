a0a2a63b0f6a7fb13903e2e98bc201e1454fb6ab
Abhinav Singh <90246402+absvn@users.noreply.github.com>
2021-12-01 17:56:59 +0530

    Create remove_and_total.py

---
+++ remove_and_total.py
@@ -0,0 +1,12 @@
+for i in folder:
+    route = '/Users/abhinavsingh/Desktop/ipa_react/processed/'+str(i)+'/main/*'
+    total = 0
+    for file in glob.glob(route):
+        total += 1
+    if total != 12:
+        print('removed :', str(i))
+        folder.remove(i)
+    else:
+        pass
+    
+    print(str(i), ':', + total)
